281c5887b03a5bfe8911dfcabc2a815794887ea2
Héctor Molinero Fernández <hector@molinero.dev>
2020-02-01 12:47:39 +0100

    Updated user agent

--- update.sh
+++ update.sh
@@ -22,7 +22,7 @@ logError() {
 }
 
 fetchUrl() {
-	curl -fsSL -A 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0' -- "$1"
+	curl -fsSL -A 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0' -- "$1"
 }
 
 removeCRLF() { tr -d '\r'; }
